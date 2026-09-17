a0695aa836c1ebc79706f5fd7ca5a62f6b68dde4
Jason Grimes <guns4america@gmail.com>
2026-09-17 01:03:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const s=a.getAttribute('href');if(s==='#')return;const x=document.querySelector(s);if(x){e.preventDefault();x.scrollIntoView({behavior:'smooth'})}}));
